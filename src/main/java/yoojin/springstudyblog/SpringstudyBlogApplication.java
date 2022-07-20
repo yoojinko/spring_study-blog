0506ea1966a55cc117dde957b031fc13af9c294f
yoojinko <db232100@gmail.com>
2022-07-20 23:23:42 +0900

    Initial commit

---
+++ src/main/java/yoojin/springstudyblog/SpringstudyBlogApplication.java
@@ -0,0 +1,13 @@
+package yoojin.springstudyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstudyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstudyBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yoojin/springstudyblog/SpringstudyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package yoojin.springstudyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
