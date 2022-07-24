e05a4af83e154ae270b638da0a8cc9e43d34172d
yoojinko <db232100@gmail.com>
2022-07-24 21:58:20 +0900

    14강 http요청 실습 2

--- src/main/java/yoojin/springstudyblog/blogtest/HttpControllerTest.java
+++ src/main/java/yoojin/springstudyblog/blogtest/HttpControllerTest.java
@@ -11,12 +11,12 @@ public class HttpControllerTest {
     //인터넷 브라우저 요청은 무조건 get 요청임.
     //postman으로 요청하면 가능
     @GetMapping("/http/get")
-    public String getTest(){
-        return "get 요청";
+    public String getTest(Member m){
+        return "get 요청 : " + m.getId() + "," + m.getUsername()+","+m.getPassword()+","+m.getEmail();
     }
     @PostMapping("/http/post")
-    public String postTest(){
-        return "post 요청";
+    public String postTest(@RequestBody Member m){
+        return "post 요청 : " + m.getId() + "," + m.getUsername()+","+m.getPassword()+","+m.getEmail();
     }
     @PutMapping("/http/put")
     public String putTest(){
