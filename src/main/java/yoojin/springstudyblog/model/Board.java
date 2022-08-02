23811131e24b7b7d35f76253cb67c95f66b499a7
yoojinko <db232100@gmail.com>
2022-08-02 23:56:08 +0900

    23강 reply 테이블 연관관계 보완

--- src/main/java/yoojin/springstudyblog/model/Board.java
+++ src/main/java/yoojin/springstudyblog/model/Board.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -34,6 +35,10 @@ public class Board {
     @JoinColumn(name="userId")
     private User user;  //DB는 오브젝트를 저장할 수 없어 FK를 사용. 자바는 오브젝트를 저장할 수 있음.
                         //=> 자바에서 DB에 맞춰 테이블을 만듦. (User의 PK로 자동 변환)
+
+    @OneToMany(mappedBy="board", fetch=FetchType.EAGER)// reply table의 board와 매핑. mappedBy -> 연관관계의 주인이 아님. (DB에 Column을 만들지 않음)
+                                                        // 기본 fetch는 LAZY이나, 게시글에서 댓글 정보를 바로 받아와야 하므로 EAGER 전략으로 변경
+    private List<Reply> reply;  // 따라서 JoinColumn은 필요없다.
 
     @CreationTimestamp
     private Timestamp createDate;
